0f7115b3cd99518c0f3198953b3a1ede92423a5c
aTran47 <97611396+aTran47@users.noreply.github.com>
2023-11-07 14:46:18 -0500

    Added onclick functionality to send intent and start activity

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
@@ -15,7 +16,14 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-
+            val sendIntent = Intent().apply{
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, editText.text)
+                type = "text/plain"
+            }
+            startActivity(Intent.createChooser(sendIntent, null))
         }
+
+
     }
 }
